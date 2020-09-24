3815034b67d8762192a58c6942aaaa74345377ef
Daniel-255 <67330373+Daniel-255@users.noreply.github.com>
2020-09-24 16:59:46 +0300

    Initial commit

---
+++ contactus.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" types="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Contact Us| e-meeting</title>
+  </head>
+  <body>
+    <div class="over-container">
+      <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h3 class="title">E-meeting</h3>
+        </div>
+        <div class="col-md-8 d-xs-none d-sm-none d-md-none d-lg-block main-menu">
+          <ul>
+            <li><a href="#">Home&nbsp; <i class="fa fa-home"></i> </a> </li>
+            <li><a href="#">About&nbsp; <i class="fa fa-question-circle"></i>  </a> </li>
+            <li><a href="#">Conact Us&nbsp; <i class="fa fa-home"></i> </a> </li>
+            <li><a href="#" class="button-lg">Login</a>&nbsp; <i class="fa fa-sign-in-alt"></i>  </li>
+          </ul>
+        </div>
+      <!--  <div class="col-md-2  d-md-none position-hd">
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fa fa-bars"></i>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </div>
+  </div><br>
+  <div class="container content-space shadow-sm p-3 mb-5 bg-white rounded">
+    <div class="row">
+      <div class="col-md-7">
+        <form class="form" action="index.html" method="post">
+          <label for="name">Full Name</label>
+          <input type="text" name="fullname" value="" required><br>
+          <label for="messsage">Your Message</label><br>
+          <textarea name="name" rows="8" cols="50"></textarea><br>
+          <button type="button" class="button" name="button">Send Message <i class="fa fa-paper-plane"></i> </button>
+        </form>
+      </div>
+      <div class="col-md-1 d-xs-none d-sm-none d-md-block">
+        <div class="vl">
+
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h3>Contact Details <hr></h3>
+            </div>
+          </div>
+              <div class="row">
+                <div class="col-md-5">
+                  <h5>Phone:</h5>
+                </div>
+                <div class="col-md-7">
+                  <h6>+255 787 209 935</h6>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-5">
+                  <h5>E-mail:</h6>
+                </div>
+                <div class="col-md-7">
+                  <h6>Helpdesk@ternet.or.net</h6>
+                </div>
+              </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 content-space shadow-sm p-3 mb-5 bg-white rounded">
+            <h3 class="header-title header-title-cg item-space">Map showing our Offices Location</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1491.4523900657714!2d39.24023666237385!3d-6.774182915053452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c4c26c93dd12f%3A0xa98951d38c560c86!2sCOSTECH!5e0!3m2!1sen!2stz!4v1600687779422!5m2!1sen!2stz" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      </div>
+    </div>
+  </div>
+  <footer>
+  <div class="over-container-ft">
+    <div class="container footer-cg">
+      <div class="row d-xs-block">
+        <div class="col-md-3">
+          <h4 style="color:#fff; font-weight:bold;">Quick Links <hr class="line"> </h4>
+          <a href="" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;Helpdesk</a><br>
+          <a href="#" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;Blog</a><br>
+          <a href="" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;Wiki</a><br>
+          <a href="#" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;FAQs |</a>
+        </div>
+        <div class="col-md-3">
+          <h4 style="color:#fff; font-weight:bold;">Quick Links <hr class="line"> </h4>
+          <a href="" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;Helpdesk</a><br>
+          <a href="#" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;Blog</a><br>
+          <a href="" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;Wiki</a><br>
+          <a href="#" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;FAQs |</a>
+        </div>
+        <div class="col-md-3">
+          <h4 style="color:#fff; font-weight:bold;">Quick Links <hr class="line"> </h4>
+          <a href="" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;Helpdesk</a><br>
+          <a href="#" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;Blog</a><br>
+          <a href="" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;Wiki</a><br>
+          <a href="#" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;FAQs |</a>
+        </div>
+        <div class="col-md-3">
+          <h4 style="color:#fff; font-weight:bold;">Location<hr class="line"> </h4>
+          <a href="" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;Helpdesk</a><br>
+          <a href="#" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;Blog</a><br>
+          <a href="" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;Wiki</a><br>
+          <a href="#" style="text-decoration:none;"> <i class="fa fa-link"></i>&nbsp;&nbsp;FAQs |</a>
+        </div>
+      </div><br>
+      <div class="row">
+        <div class="col-md-4" class="position">
+          <a href="#">About</a>&nbsp;
+          <a href="#">Privacy Policy</a>&nbsp;
+          <a href="#">Join Us</a>
+        </div>
+        <div class="col-md-4">
+          <p class="position">Ternet | E-meeting &#169;2020</p>
+        </div>
+        <div class="col-md-4">
+          <a href="#">Register Membership</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content= "width=device-width, initial-scale=1.0">
+    <title>e-meeting</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" types="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="over-container">
+      <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h3 class="title"><a href="#">E-meeting</a></h3>
+        </div>
+        <div class="col-md-8 d-xs-none d-sm-none d-md-none d-lg-block main-menu">
+          <ul>
+            <li><a href="#">Home&nbsp; <i class="fa fa-home"></i> </a> </li>
+            <li><a href="#">About&nbsp; <i class="fa fa-question-circle"></i>  </a> </li>
+            <li><a href="contactus.php">Conact Us&nbsp; <i class="fa fa-envelope"></i> </a> </li>
+            <li><a href="#" class="button-lg">Login&nbsp;<i class="fa fa-user-unlock"></i></a></li>
+          </ul>
+        </div>
+      <!--  <div class="col-md-2  d-md-none position-hd">
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fa fa-bars"></i>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </div>
+  </div>
+    <div class="container container-space">
+      <div class="row ">
+        <div class="col-md-12">
+                      <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="sys1.jpg" class="d-block w-100" height="470" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <button type="button" class="button-vd" name="button">Learn More</button>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="meeting.jpg" class="d-block w-100" height="470" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <button type="button" class="button-vd" name="button">Learn More</button>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="director.png" class="d-block w-100" height="470" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <button type="button" class="button-vd" name="button">Learn More</button>
+                  </div>
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 box-card shadow-sm p-3 mb-5 bg-white rounded">
+          <h3>E-meeting <hr> </h3>
+          <p class="card-text">    Some quick example text to build on the card title and make up the bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button type="button" class="button button-width" name="button"><span>Learn More</span></button>
+        </div>
+
+        <div class="col-md-3 box-card shadow-sm p-3 mb-5 bg-white rounded">
+          <h3>E-meeting <hr> </h3>
+          <p class="card-text">    Some quick example text to build on the card title and make up the bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button type="button" class="button button-width" name="button"><span>Learn More</span></button>
+        </div>
+
+        <div class="col-md-3 box-card shadow-sm p-3 mb-5 bg-white rounded">
+          <h3>E-meeting <hr> </h3>
+          <p class="card-text">    Some quick example text to build on the card title and make up the bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button type="button" class="button button-width" name="button"><span>Learn More</span></button>
+        </div>
+
+
+        <div class="col-md-3 box-card shadow-sm p-3 mb-5 bg-white rounded">
+          <h3>E-meeting <hr> </h3>
+          <p class="card-text">    Some quick example text to build on the card title and make up the bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button type="button" class="button button-width" name="button"><span>Learn More</span></button>
+        </div>
+
+
+      </div>
+    </div>
+    </div><br>
+    <div class="container content-bg shadow-sm p-3 mb-5 bg-white rounded">
+      <div class="row row-space">
+        <div class="col-md-6">
+          <h4>About E-meeting</h4>
+          <hr>
+          <p class="card-text card-txt-color">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+            Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button class="button"><span>Learn More</span></button>
+        </div>
+        <div class="col-md-6 item-space">
+          <img src="sys1.jpg" class="img-fluid" height="auto" alt="">
+        </div>
+      </div>
+    </div><br>
+    <div class="container content-bg shadow-sm p-3 mb-5 bg-white rounded">
+      <div class="row row-space">
+        <div class="col-md-6">
+          <img src="sys1.jpg" class="img-fluid" height="auto" alt="">
+        </div>
+        <div class="col-md-6 item-space">
+          <h4>About E-meeting</h4>
+          <hr>
+          <p class="card-text card-txt-color">Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content.
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+            Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button class="button"><span>Learn More</span></button>
+        </div>
+      </div>
+    </div><br>
+    <div class="container content-bg shadow-sm p-3 mb-5 bg-white rounded">
+      <div class="row row-space">
+        <h3 class="header-title header-title-cg">A short video About E-meeting</h3>
+        <iframe class="embeded-responsive-item vd-style" width="1200" height="400" src="https://www.youtube.com/embed/0tVTq642hTo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <!--<iframe class="embeded-responsive-item vd-style" src="https://youtu.be/0tVTq642hTo" class="img-fluid" width="1200" height="400" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>-->
+      </div>
+    </div><br>
+    <div class="container content-bg shadow-sm p-3 mb-5 bg-white rounded">
+      <div class="row row-space">
+        <div class="col-md-12">
+            <h4 class="header-title-cg">Testmonials</h4>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <p class="card-text card-txt-color position"> <i class="fa fa-quote-left"></i>
+                      Some quick example text to build on the card title and make up the bulk of the card's content.<br>
+                      Some quick example text to build on the card title and make up the bulk of the card's content.<br>
+                      Some quick example text to build on the card title and make up the bulk of the card's content.
+                      Some quick example text to build on the card title and make up the bulk of the card's content.
+                      <i class="fa fa-quote-right"></i>
+                      <h5 class="header-title-cg">Mr Olango John <p class="p-cg">Same institute</p> </h5><br>
+                    </p>
+                </div>
+                <div class="carousel-item">
+                  <p class="card-text card-txt-color position"> <i class="fa fa-quote-left"></i>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.<br>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.<br>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    <i class="fa fa-quote-right"></i><br>
+                    <h5 class="header-title-cg">Mr Olango John <p class="p-cg">Same institute</p> </h5><br>
+                  </p>
+                </div>
+                <div class="carousel-item">
+                  <p class="card-text card-txt-color position"> <i class="fa fa-quote-left"></i>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.<br>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.<br>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    <i class="fa fa-quote-right"></i>
+                    <h5 class="header-title-cg">Mr Olango John <p class="p-cg">Same institute</p> </h5><br>
+                  </p>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div><br>
+    <footer>
+    <div class="over-container-ft">
+      <div class="container footer-cg ">
+        <div class="row d-none d-sm-block d-md-none">
+          <div class="col-md-3">
+            <h4 class="header-title header-title-cg">Quick Links <hr class="line"> </h4>
+            <a href="" class="text-1"> <i class="fa fa-link"></i>&nbsp;Helpdesk</a><br>
+            <a href="#" class="text-1"> <i class="fa fa-link"></i>&nbsp;&nbsp;Blog</a><br>
+            <a href="" class="text-1"> <i class="fa fa-link"></i>&nbsp;&nbsp;Wiki</a><br>
+            <a href="#" class="text-1"> <i class="fa fa-link"></i>&nbsp;&nbsp;FAQs |</a>
+          </div>
+          <div class="col-md-3">
+            <h4 class="header-title header-title-cg">Quick Links <hr class="line"> </h4>
+            <a href="" class="text-1"> <i class="fa fa-link"></i>&nbsp;Helpdesk</a><br>
+            <a href="#" class="text-1"> <i class="fa fa-link"></i>&nbsp;&nbsp;Blog</a><br>
+            <a href="" class="text-1"> <i class="fa fa-link"></i>&nbsp;&nbsp;Wiki</a><br>
+            <a href="#" class="text-1"> <i class="fa fa-link"></i>&nbsp;&nbsp;FAQs |</a>
+          </div>
+          <div class="col-md-3">
+            <h4 class="header-title header-title-cg">Quick Links <hr class="line"> </h4>
+            <a href="" class="text-1"> <i class="fa fa-link"></i>&nbsp;Helpdesk</a><br>
+            <a href="#" class="text-1"> <i class="fa fa-link"></i>&nbsp;&nbsp;Blog</a><br>
+            <a href="" class="text-1"> <i class="fa fa-link"></i>&nbsp;&nbsp;Wiki</a><br>
+            <a href="#" class="text-1"> <i class="fa fa-link"></i>&nbsp;&nbsp;FAQs |</a>
+          </div>
+          <div class="col-md-3">
+            <h4 class="header-title header-title-cg">Location<hr class="line"> </h4>
+            <a1 class="text-1">Kijitonyama, Bagamoyo road</a1><br>
+            <a1 class="text-1">COSTECH Building</a1><br>
+            <a1 class="text-1">FAQs |</a1>
+          </div>
+        </div><br>
+        <div class="row">
+          <div class="col-md-4">
+            <a href="#">About</a>&nbsp;
+            <a href="#">Privacy Policy</a>&nbsp;
+            <a href="#">Join Us</a>
+          </div>
+          <div class="col-md-4">
+            <p>Ternet | E-meeting &#169;2020</p>
+          </div>
+          <div class="col-md-4">
+            <a href="#">Register Membership</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </footer>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    </body>
+</html>
